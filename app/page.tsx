de2472d76e8abe39643254c33ec12aa4912c41c6
Finn <finn@fsj.xyz>
2024-07-04 19:22:05 -0400

    Learning the ropes 2

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,13 @@ export default function Page() {
       </p>
       <p className="mb-4">
         {`I am a software engineer and amateur artist based in Durham, NC. I also build `}
-        <a className="underline decoration-neutral-400 dark:decoration-neutral-600 underline-offset-2 decoration-[0.1em] hover:text-blue-600 hover:decoration-blue-400" href='https://github.com/finnjames/threepio'>{`systems`}</a>
+        <a className="underline decoration-neutral-400 dark:decoration-neutral-600 underline-offset-2 decoration-[0.1em] hover:text-blue-600 hover:decoration-blue-400" href='https://github.com/finnjames/threepio'>
+          {`systems`}
+        </a>
         {` for `}
-        <a className="underline decoration-neutral-400 dark:decoration-neutral-600 underline-offset-2 decoration-[0.1em] hover:text-blue-600 hover:decoration-blue-400" href='https://skynet.unc.edu/'>{`telescopes`}</a>
+        <a className="underline decoration-neutral-400 dark:decoration-neutral-600 underline-offset-2 decoration-[0.1em] hover:text-blue-600 hover:decoration-blue-400" href='https://skynet.unc.edu/'>
+          {`telescopes`}
+        </a>
         {`.`}
       </p>
       <div className="my-8">
